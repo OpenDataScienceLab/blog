cfe9ad503d81f685f8e0dc09201afa035c33c762
Sandro Loch <es.loch@gmail.com>
2019-07-08 18:16:21 -0300

    Nikola auto commit.
    Nikola version: 8.0.2

--- conf.py
+++ conf.py
@@ -649,7 +649,11 @@ REDIRECTIONS = []
 
 # The name of the remote where you wish to push to, using github_deploy.
 GITHUB_REMOTE_NAME = 'origin'
+
+GITHUB_SOURCE_BRANCH = 'src'
+
 GITHUB_DEPLOY_BRANCH = 'gh-pages'
+
 
 # Whether or not github_deploy should commit to the source branch automatically
 # before deploying.
